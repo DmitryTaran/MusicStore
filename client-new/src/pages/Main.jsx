7df325442cc23b9c1b94e26ea025f10ec7811d75
DmitryTaran <dmitryvik55@gmail.com>
2022-10-20 13:40:21 +0300

    FirstPushOnGitHub

--- client-new/src/pages/Main.jsx
+++ client-new/src/pages/Main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import TypeBar from "../components/TypeBar/TypeBar";
-import DeviceItem from "../components/DeviceItem/DeviceItem";
 import DeviceList from "../components/DeviceList/DeviceList";
-import Button from "../components/UI/Button/Button";
 
 const Main = () => {
 
